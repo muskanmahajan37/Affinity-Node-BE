aecb6aaa31acff9474562c3594af6fd3946be8f9
royal2710 <royal2710@outlook.com>
2019-06-20 03:23:52 +0800

    client api fix

--- Http/Models/Client.js
+++ Http/Models/Client.js
@@ -21,15 +21,24 @@ exports.get_clients = function(req, res) {
                     .query('SELECT ClientId FROM schedules WHERE SocialSecNum = @ssn');
     }).then(result => {
         sql.close();
-        console.log('=== get clients ids(in schedules table) ===', result.rowsAffected);
-        if(result.recordset.length > 0) {
-            var ids = '';
+        console.log('=== get clients ids(in schedules table) ===', result);
+        if(result.rowsAffected[0] > 0) {
+            // var ids = '';
+            // for (var i=0; i<result.recordset.length; i++) {
+            //     if (!ids.includes(', ' + result.recordset[i].ClientId)) {
+            //         ids += ', ' + result.recordset[i].ClientId;
+            //     }
+            // }
+            // ids = ids.replace(', ', '');
+            // get_clients_by_ids(req, res, ids);
+            var ids = [];
             for (var i=0; i<result.recordset.length; i++) {
-                if (!ids.indexOf(result.recordset[i].ClientId + ', ')) {
-                    ids += i == (result.recordset.length - 1) ? (result.recordset[i].ClientId) : (result.recordset[i].ClientId + ', ');
+                if (!ids.includes(result.recordset[i].ClientId)) {
+                    ids.push(result.recordset[i].ClientId);
                 }
             }
-            get_clients_by_ids(req, res, ids);
+            var data = [];
+            get_clients_by_ids(req, res, ids, 0, data);
         } else {
             res.status(400).send({status: 1, data: JSON.stringify(result.recordset), msg: 'There is no actived client.'});
         }
@@ -40,20 +49,29 @@ exports.get_clients = function(req, res) {
     });
 }
 
-get_clients_by_ids = function (req, res, ids) {
+get_clients_by_ids = function (req, res, ids, index, data) {
     console.log('=== unique client ids ===', ids);
+    var _i = parseInt(index);
+    var _data = data;
     sql.connect(config).then(pool => {
         return pool.request()
                     .input('status', sql.NVarChar, 'A')
-                    .input('ids', sql.NVarChar, ids)
-                    .query('SELECT ClientId, FirstName, LastName FROM client WHERE Status = @status AND ClientId in (@ids)');
+                    .input('id', sql.Int, parseInt(ids[_i]))
+                    .query('SELECT ClientId, FirstName, LastName FROM client WHERE Status = @status AND ClientId = @id');
     }).then(result => {
         sql.close();
         console.log('=== get clients by id - result ===', result.rowsAffected);
         if(result.recordset.length > 0) {
-            res.status(200).send({status: 0, data: JSON.stringify(result.recordset), msg: ''});
+            _data.push(result.recordset[0]);
+        }
+        if (_i < ids.length) {
+            get_clients_by_ids(req, res, ids, _i + 1, _data);
         } else {
-            res.status(400).send({status: 1, data: JSON.stringify(result.recordset), msg: 'There is no actived client.'});
+            if (_data.length > 0) {
+                res.status(200).send({status: 0, data: JSON.stringify(_data), msg: ''});
+            } else {
+                res.status(400).send({status: 1, data: JSON.stringify(result.recordset), msg: 'There is no actived client.'});
+            }
         }
     }).catch(err => {
         console.log('=== get clients by ids - catch err ===', err);
